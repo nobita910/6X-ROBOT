b5ecb355bd5001c4df42915d249209dec9c6e5f3
NOBITA-CHOWDHURY <162557199+nobita910@users.noreply.github.com>
2024-04-10 17:23:45 +0600

    Update hadis.js

--- modules/commands/hadis.js
+++ modules/commands/hadis.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "hadis",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "AYAN",
+	credits: "SALMAN",
 	description: "Islamic Hadis.",
 	commandCategory: "Religious",
 	cooldowns: 5
