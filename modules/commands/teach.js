d872ca221aed5aec6353ec7dee8da0446ed8a662
NOBITA-CHOWDHURY <162557199+nobita910@users.noreply.github.com>
2024-04-12 10:55:46 +0600

    Update teach.js

--- modules/commands/teach.js
+++ modules/commands/teach.js
@@ -4,7 +4,7 @@ module.exports.config = {
   name: "teach",
   version: "1.0.0",
   permssion: 0,
-  credits: "AYAN CHOWDHURY",//don’t change the credits please
+  credits: "𝗦𝗛𝟰𝗪𝗢𝗡 CHOWDHURY",//don’t change the credits please
   usePrefix: true,
   description: "Teach AI",
   commandCategory: " SIM ✅",
